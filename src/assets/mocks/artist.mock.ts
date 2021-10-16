701f28ca2abd8a485a9fb9f41bf5f4392d611f5e
bgrinste <bgrinste@netapp.com>
2021-10-16 19:53:22 +0300

    main component creation, spotify service first configs

---
+++ src/assets/mocks/artist.mock.ts
@@ -0,0 +1,417 @@
+export const mock = {
+  "artists": {
+  "href": "https://api.spotify.com/v1/search?query=John+legend&type=artist&offset=0&limit=30",
+    "items": [
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/5y2Xq6xcjJb2jVM54GHK3t"
+      },
+      "followers": {
+        "href": null,
+        "total": 5926389
+      },
+      "genres": [
+        "neo soul",
+        "pop",
+        "pop soul",
+        "r&b",
+        "urban contemporary"
+      ],
+      "href": "https://api.spotify.com/v1/artists/5y2Xq6xcjJb2jVM54GHK3t",
+      "id": "5y2Xq6xcjJb2jVM54GHK3t",
+      "images": [
+        {
+          "height": 640,
+          "url": "https://i.scdn.co/image/ab6761610000e5eb8f2bf07f58f571ae430ff1be",
+          "width": 640
+        },
+        {
+          "height": 320,
+          "url": "https://i.scdn.co/image/ab676161000051748f2bf07f58f571ae430ff1be",
+          "width": 320
+        },
+        {
+          "height": 160,
+          "url": "https://i.scdn.co/image/ab6761610000f1788f2bf07f58f571ae430ff1be",
+          "width": 160
+        }
+      ],
+      "name": "John Legend",
+      "popularity": 81,
+      "type": "artist",
+      "uri": "spotify:artist:5y2Xq6xcjJb2jVM54GHK3t"
+    },
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/3UgTNJqLIdu81sUFXOgA5R"
+      },
+      "followers": {
+        "href": null,
+        "total": 446
+      },
+      "genres": [],
+      "href": "https://api.spotify.com/v1/artists/3UgTNJqLIdu81sUFXOgA5R",
+      "id": "3UgTNJqLIdu81sUFXOgA5R",
+      "images": [
+        {
+          "height": 640,
+          "url": "https://i.scdn.co/image/ab67616d0000b273f0e10994bb1dcde403e16f95",
+          "width": 640
+        },
+        {
+          "height": 300,
+          "url": "https://i.scdn.co/image/ab67616d00001e02f0e10994bb1dcde403e16f95",
+          "width": 300
+        },
+        {
+          "height": 64,
+          "url": "https://i.scdn.co/image/ab67616d00004851f0e10994bb1dcde403e16f95",
+          "width": 64
+        }
+      ],
+      "name": "Johnny Legend",
+      "popularity": 5,
+      "type": "artist",
+      "uri": "spotify:artist:3UgTNJqLIdu81sUFXOgA5R"
+    },
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/3AZN0ouBPEjOOFKNSAdhKB"
+      },
+      "followers": {
+        "href": null,
+        "total": 70
+      },
+      "genres": [],
+      "href": "https://api.spotify.com/v1/artists/3AZN0ouBPEjOOFKNSAdhKB",
+      "id": "3AZN0ouBPEjOOFKNSAdhKB",
+      "images": [],
+      "name": "John H. Legend",
+      "popularity": 0,
+      "type": "artist",
+      "uri": "spotify:artist:3AZN0ouBPEjOOFKNSAdhKB"
+    },
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/1JSfEIaAV2aBS0xBIqOyEc"
+      },
+      "followers": {
+        "href": null,
+        "total": 8
+      },
+      "genres": [],
+      "href": "https://api.spotify.com/v1/artists/1JSfEIaAV2aBS0xBIqOyEc",
+      "id": "1JSfEIaAV2aBS0xBIqOyEc",
+      "images": [],
+      "name": "The Legendary Johnny Guitar",
+      "popularity": 0,
+      "type": "artist",
+      "uri": "spotify:artist:1JSfEIaAV2aBS0xBIqOyEc"
+    },
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/5JIEtA19PGWozfy8haZceV"
+      },
+      "followers": {
+        "href": null,
+        "total": 34
+      },
+      "genres": [],
+      "href": "https://api.spotify.com/v1/artists/5JIEtA19PGWozfy8haZceV",
+      "id": "5JIEtA19PGWozfy8haZceV",
+      "images": [
+        {
+          "height": 640,
+          "url": "https://i.scdn.co/image/ab67616d0000b2730e80d31276b98683d01fd14b",
+          "width": 640
+        },
+        {
+          "height": 300,
+          "url": "https://i.scdn.co/image/ab67616d00001e020e80d31276b98683d01fd14b",
+          "width": 300
+        },
+        {
+          "height": 64,
+          "url": "https://i.scdn.co/image/ab67616d000048510e80d31276b98683d01fd14b",
+          "width": 64
+        }
+      ],
+      "name": "Syl Johnson The Legend",
+      "popularity": 0,
+      "type": "artist",
+      "uri": "spotify:artist:5JIEtA19PGWozfy8haZceV"
+    },
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/0OlJqz1lJpbwQag8b4E3sy"
+      },
+      "followers": {
+        "href": null,
+        "total": 44
+      },
+      "genres": [],
+      "href": "https://api.spotify.com/v1/artists/0OlJqz1lJpbwQag8b4E3sy",
+      "id": "0OlJqz1lJpbwQag8b4E3sy",
+      "images": [],
+      "name": "Juma Featuring John Legend",
+      "popularity": 0,
+      "type": "artist",
+      "uri": "spotify:artist:0OlJqz1lJpbwQag8b4E3sy"
+    },
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/22TsjBnkQP5ZD3xS5DBpAR"
+      },
+      "followers": {
+        "href": null,
+        "total": 11
+      },
+      "genres": [],
+      "href": "https://api.spotify.com/v1/artists/22TsjBnkQP5ZD3xS5DBpAR",
+      "id": "22TsjBnkQP5ZD3xS5DBpAR",
+      "images": [],
+      "name": "Syl Johnson's Legendary Band",
+      "popularity": 0,
+      "type": "artist",
+      "uri": "spotify:artist:22TsjBnkQP5ZD3xS5DBpAR"
+    },
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/4L07IaEqMHSTvn8AfrRVQB"
+      },
+      "followers": {
+        "href": null,
+        "total": 5
+      },
+      "genres": [],
+      "href": "https://api.spotify.com/v1/artists/4L07IaEqMHSTvn8AfrRVQB",
+      "id": "4L07IaEqMHSTvn8AfrRVQB",
+      "images": [
+        {
+          "height": 640,
+          "url": "https://i.scdn.co/image/ab67616d0000b27381df543a7e44055be2389425",
+          "width": 640
+        },
+        {
+          "height": 300,
+          "url": "https://i.scdn.co/image/ab67616d00001e0281df543a7e44055be2389425",
+          "width": 300
+        },
+        {
+          "height": 64,
+          "url": "https://i.scdn.co/image/ab67616d0000485181df543a7e44055be2389425",
+          "width": 64
+        }
+      ],
+      "name": "The Legendary Miles Johnson",
+      "popularity": 0,
+      "type": "artist",
+      "uri": "spotify:artist:4L07IaEqMHSTvn8AfrRVQB"
+    },
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/0LBHyUZD5PKjdqGPKArWBt"
+      },
+      "followers": {
+        "href": null,
+        "total": 0
+      },
+      "genres": [],
+      "href": "https://api.spotify.com/v1/artists/0LBHyUZD5PKjdqGPKArWBt",
+      "id": "0LBHyUZD5PKjdqGPKArWBt",
+      "images": [],
+      "name": "John Bowie Legendary DJ Kool",
+      "popularity": 0,
+      "type": "artist",
+      "uri": "spotify:artist:0LBHyUZD5PKjdqGPKArWBt"
+    },
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/6hrDzQ5iyY21BcE372Jkvr"
+      },
+      "followers": {
+        "href": null,
+        "total": 10
+      },
+      "genres": [],
+      "href": "https://api.spotify.com/v1/artists/6hrDzQ5iyY21BcE372Jkvr",
+      "id": "6hrDzQ5iyY21BcE372Jkvr",
+      "images": [
+        {
+          "height": 640,
+          "url": "https://i.scdn.co/image/ab67616d0000b273873af20bff3a3e3f95da1d3f",
+          "width": 640
+        },
+        {
+          "height": 300,
+          "url": "https://i.scdn.co/image/ab67616d00001e02873af20bff3a3e3f95da1d3f",
+          "width": 300
+        },
+        {
+          "height": 64,
+          "url": "https://i.scdn.co/image/ab67616d00004851873af20bff3a3e3f95da1d3f",
+          "width": 64
+        }
+      ],
+      "name": "Legends of Ska - Cluett Johnson",
+      "popularity": 0,
+      "type": "artist",
+      "uri": "spotify:artist:6hrDzQ5iyY21BcE372Jkvr"
+    },
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/0m396sHHQSMMEfTrsYFMVK"
+      },
+      "followers": {
+        "href": null,
+        "total": 2
+      },
+      "genres": [],
+      "href": "https://api.spotify.com/v1/artists/0m396sHHQSMMEfTrsYFMVK",
+      "id": "0m396sHHQSMMEfTrsYFMVK",
+      "images": [],
+      "name": "Johnny Legend & His Rockabilly Bastards",
+      "popularity": 0,
+      "type": "artist",
+      "uri": "spotify:artist:0m396sHHQSMMEfTrsYFMVK"
+    },
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/5OpZx7nB5pnFZ4WqhCUZTF"
+      },
+      "followers": {
+        "href": null,
+        "total": 23
+      },
+      "genres": [],
+      "href": "https://api.spotify.com/v1/artists/5OpZx7nB5pnFZ4WqhCUZTF",
+      "id": "5OpZx7nB5pnFZ4WqhCUZTF",
+      "images": [
+        {
+          "height": 640,
+          "url": "https://i.scdn.co/image/ab67616d0000b27387d155a109875e5eb90fe8b6",
+          "width": 640
+        },
+        {
+          "height": 300,
+          "url": "https://i.scdn.co/image/ab67616d00001e0287d155a109875e5eb90fe8b6",
+          "width": 300
+        },
+        {
+          "height": 64,
+          "url": "https://i.scdn.co/image/ab67616d0000485187d155a109875e5eb90fe8b6",
+          "width": 64
+        }
+      ],
+      "name": "John Girgus and the Legendary House Cats",
+      "popularity": 0,
+      "type": "artist",
+      "uri": "spotify:artist:5OpZx7nB5pnFZ4WqhCUZTF"
+    },
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/2ibvHu1sjYOf2xMIMuyN2n"
+      },
+      "followers": {
+        "href": null,
+        "total": 3
+      },
+      "genres": [],
+      "href": "https://api.spotify.com/v1/artists/2ibvHu1sjYOf2xMIMuyN2n",
+      "id": "2ibvHu1sjYOf2xMIMuyN2n",
+      "images": [
+        {
+          "height": 640,
+          "url": "https://i.scdn.co/image/ab67616d0000b2732e214d17df67b2316af36418",
+          "width": 640
+        },
+        {
+          "height": 300,
+          "url": "https://i.scdn.co/image/ab67616d00001e022e214d17df67b2316af36418",
+          "width": 300
+        },
+        {
+          "height": 64,
+          "url": "https://i.scdn.co/image/ab67616d000048512e214d17df67b2316af36418",
+          "width": 64
+        }
+      ],
+      "name": "Johnny Oklahoma and the Legendary Tb Slick Revue",
+      "popularity": 0,
+      "type": "artist",
+      "uri": "spotify:artist:2ibvHu1sjYOf2xMIMuyN2n"
+    },
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/5PdDidCrXRgYchbsUy27yW"
+      },
+      "followers": {
+        "href": null,
+        "total": 144
+      },
+      "genres": [],
+      "href": "https://api.spotify.com/v1/artists/5PdDidCrXRgYchbsUy27yW",
+      "id": "5PdDidCrXRgYchbsUy27yW",
+      "images": [
+        {
+          "height": 640,
+          "url": "https://i.scdn.co/image/ab67616d0000b273f44fc10ec239b49081d065da",
+          "width": 640
+        },
+        {
+          "height": 300,
+          "url": "https://i.scdn.co/image/ab67616d00001e02f44fc10ec239b49081d065da",
+          "width": 300
+        },
+        {
+          "height": 64,
+          "url": "https://i.scdn.co/image/ab67616d00004851f44fc10ec239b49081d065da",
+          "width": 64
+        }
+      ],
+      "name": "Tribute To John Legend",
+      "popularity": 0,
+      "type": "artist",
+      "uri": "spotify:artist:5PdDidCrXRgYchbsUy27yW"
+    },
+    {
+      "external_urls": {
+        "spotify": "https://open.spotify.com/artist/2B9bKdoyXqhmKwi45uq0Vf"
+      },
+      "followers": {
+        "href": null,
+        "total": 0
+      },
+      "genres": [],
+      "href": "https://api.spotify.com/v1/artists/2B9bKdoyXqhmKwi45uq0Vf",
+      "id": "2B9bKdoyXqhmKwi45uq0Vf",
+      "images": [
+        {
+          "height": 640,
+          "url": "https://i.scdn.co/image/ab67616d0000b2738c22d6c6f5cf494c021b08ac",
+          "width": 640
+        },
+        {
+          "height": 300,
+          "url": "https://i.scdn.co/image/ab67616d00001e028c22d6c6f5cf494c021b08ac",
+          "width": 300
+        },
+        {
+          "height": 64,
+          "url": "https://i.scdn.co/image/ab67616d000048518c22d6c6f5cf494c021b08ac",
+          "width": 64
+        }
+      ],
+      "name": "Father Johnwell Peterside Jr & De Legends",
+      "popularity": 0,
+      "type": "artist",
+      "uri": "spotify:artist:2B9bKdoyXqhmKwi45uq0Vf"
+    }
+  ],
+    "limit": 30,
+    "next": null,
+    "offset": 0,
+    "previous": null,
+    "total": 15
+}
+}
