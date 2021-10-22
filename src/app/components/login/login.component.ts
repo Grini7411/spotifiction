4e2ab5d5e17429675a4c9dd06787d70c3d8bb67a
bgrinste <bgrinste@netapp.com>
2021-10-22 19:44:16 +0300

    pass login mechanism to own component

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {SpotifyService} from "../../services/spotify.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private spotifyServ: SpotifyService) { }
+
+  ngOnInit(): void {
+  }
+
+  getToken() {
+    this.spotifyServ.getAccessToken();
+  }
+
+}
